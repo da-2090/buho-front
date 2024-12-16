00a976c35c4106963847c1aac3c9f61ec3f328e3
Diego Carrasco <diego.carrasco@outlook.com>
2024-12-16 10:53:40 -0300

    add comments

--- src/app/api/quotation/route.ts
+++ src/app/api/quotation/route.ts
@@ -4,9 +4,11 @@ const getProductDetails = async (externalId: string): Promise<Product[]> => {
   let response = await fetch(`https://staging.buhochile.com/products/semantikos/${externalId}`);
   let products: Product[] = await response.json();
 
+  // Si se encontraron productos y el semantikosMcId es diferente al externalId proporcionado, es posible que se 
+  // buscara por producto y no por principio activo por lo que se busca nuevamente por semantikosMcId que corresponde al medicamento
   if (products.length > 0 && products[0].semantikosMcId && products[0].semantikosMcId !== externalId) {
-      response = await fetch(`https://staging.buhochile.com/products/semantikos/${products[0].semantikosMcId}`);
-      products = await response.json();
+    response = await fetch(`https://staging.buhochile.com/products/semantikos/${products[0].semantikosMcId}`);
+    products = await response.json();
   }
 
   return products;
@@ -16,10 +18,15 @@ const getProductPrices = async (productId: string, requiredQuantity: number, tab
   const response = await fetch(`https://staging.buhochile.com/products/${productId}/prices`);
   const data = await response.json();
   const prices: Price[] = data;
+
+  // Calcula el número de cajas requeridas basándose en la cantidad necesaria y las tabletas por caja
   const requiredBoxes = Math.ceil(requiredQuantity / tabletsPerBox);
+
+  // Filtra los precios para incluir solo aquellos con stock suficiente
   return prices.filter(price => price.stock >= requiredBoxes);
 };
 
+// Función para calcular la cantidad total de tabletas necesarias para un tratamiento
 const calculateTabletsQuantity = (unitsPerDay: number, duration: number, unit: 'day' | 'week' | 'month'): number => {
   const days = unit === 'day' ? duration : unit === 'week' ? duration * 7 : duration * 30;
   return Math.ceil(unitsPerDay * days);
@@ -29,72 +36,76 @@ const generateQuotation = async (prescription: Prescription): Promise<Quotation>
   let totalMinPrice = 0;
   const products: QuotationProduct[] = [];
 
+  // Se obtienen los detalles de los productos para cada ítem de la prescripción de manera concurrente
   const productDetailsPromises = prescription.prescriptionItems.map(item => getProductDetails(item.externalId));
   const productDetailsList = await Promise.all(productDetailsPromises);
 
+  // se genera la cotización para cada item de la prescripción
   for (let i = 0; i < prescription.prescriptionItems.length; i++) {
-      const item = prescription.prescriptionItems[i];
-      const productList = productDetailsList[i];
+    const item = prescription.prescriptionItems[i];
+    const productList = productDetailsList[i];
 
-      const sortedProducts = productList.sort((a, b) => (a.minPrice/a.tablets) - (b.minPrice/b.tablets));
-      const cheapestProduct = sortedProducts[0];
+    // Se ordenan los productos por precio por tableta y se selecciona el más barato
+    const sortedProducts = productList.sort((a, b) => (a.minPrice / a.tablets) - (b.minPrice / b.tablets));
+    const cheapestProduct = sortedProducts[0];
 
-      const tabletsQuantity = calculateTabletsQuantity(item.unitsPerDay, item.treatmentDuration, item.treatmentDurationUnit);
-      const productQuantity = Math.ceil(tabletsQuantity / cheapestProduct.tablets);
+    // Se calcula la cantidad de tabletas necesarias y la cantidad de productos a comprar
+    const tabletsQuantity = calculateTabletsQuantity(item.unitsPerDay, item.treatmentDuration, item.treatmentDurationUnit);
+    const productQuantity = Math.ceil(tabletsQuantity / cheapestProduct.tablets);
 
-      const prices = await getProductPrices(cheapestProduct.id, productQuantity, cheapestProduct.tablets);
-      const cheapestPrice = Math.min(...prices.map(p => p.price));
+    // Se obtienen los precios de los productos y se calcula el precio total
+    const prices = await getProductPrices(cheapestProduct.id, productQuantity, cheapestProduct.tablets);
+    const cheapestPrice = Math.min(...prices.map(p => p.price));
 
-      const alternatives = sortedProducts.slice(1, 3).map(product => ({
-          id: product.id,
-          externalId: product.externalId,
-          name: product.name,
-          concentration: product.concentration,
-          minPrice: product.minPrice,
-          tablets: product.tablets,
-          pharmaceuticForm: product.pharmaceuticForm,
-          activePrinciple: product.activePrinciple,
-          lab: product.lab ?? 'AltLab',
-          image: product.image ?? null
-      }));
+    const selectedProduct = {
+      id: cheapestProduct.id,
+      externalId: item.externalId,
+      name: cheapestProduct.name,
+      minPrice: cheapestPrice,
+      tablets: cheapestProduct.tablets,
+      pharmaceuticForm: cheapestProduct.pharmaceuticForm,
+      activePrinciple: cheapestProduct.activePrinciple,
+      concentration: cheapestProduct.concentration,
+      lab: cheapestProduct.lab ?? 'pillsLab',
+      image: cheapestProduct.image ?? null,
+    };
 
-      const selectedProduct = {
-        id: cheapestProduct.id,
-        externalId: item.externalId,
-        name: cheapestProduct.name,
-        minPrice: cheapestPrice,
-        tablets: cheapestProduct.tablets,
-        pharmaceuticForm: cheapestProduct.pharmaceuticForm,
-        activePrinciple: cheapestProduct.activePrinciple,
-        concentration: cheapestProduct.concentration,
-        lab: cheapestProduct.lab ?? 'pillsLab',
-        image: cheapestProduct.image ?? null,
-      };
+    const alternatives = sortedProducts.slice(1, 3).map(product => ({
+      id: product.id,
+      externalId: product.externalId,
+      name: product.name,
+      concentration: product.concentration,
+      minPrice: product.minPrice,
+      tablets: product.tablets,
+      pharmaceuticForm: product.pharmaceuticForm,
+      activePrinciple: product.activePrinciple,
+      lab: product.lab ?? 'AltLab',
+      image: product.image ?? null
+    }));
 
-      products.push({
-        prescription: item,
-        selectedProduct,
-        productQuantity,
-        totalPrice: cheapestPrice * productQuantity,
-        alternatives,
-      });
+    products.push({
+      prescription: item,
+      selectedProduct,
+      productQuantity,
+      totalPrice: cheapestPrice * productQuantity,
+      alternatives,
+    });
 
-      totalMinPrice += cheapestPrice * productQuantity;
+    totalMinPrice += cheapestPrice * productQuantity;
   }
 
   return {
-      patient: prescription.name,
-      minPrice: totalMinPrice,
-      productQuantity: products.length,
-      products,
+    patient: prescription.name,
+    minPrice: totalMinPrice,
+    productQuantity: products.length,
+    products,
   };
 };
 
 
 
 export async function POST(req: Request) {
-
-  const {name, address, prescription} = await req.json();
+  const { name, address, prescription } = await req.json();
 
   const prescriptionData: Prescription = {
     name,
@@ -107,8 +118,8 @@ export async function POST(req: Request) {
 }
 
 
-
-const tytoResponse:Prescription = {
+//desde aca solo sirve para mostrar como mock la api desde el navegador
+const tytoResponse: Prescription = {
   name: "Leonidas Mosby",
   address: {
     address1: "150 W. 85th Street",
